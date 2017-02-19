e8e85a9f8f754a121af3805b023e6fbbd986394d
Aaron Jenner <aaronjenner13@gmail.com>
2017-02-19 15:57:13 +0000

    Finished File System :100:

--- web/controllers/file_controller.ex
+++ web/controllers/file_controller.ex
@@ -38,4 +38,56 @@ defmodule Dafs.FileController do
     end
   end
 
+  def show(conn, %{"id" => id}) do
+    current_user_id = Coherence.current_user(conn).id
+    file = Repo.get!(DFile, id)
+    case file.user_id do
+      ^current_user_id ->
+        contents = file.contents
+        conn
+        |> put_resp_content_type("application/octet-stream", nil)
+        |> put_resp_header("content-disposition", ~s[attachment; filename="#{file.name}"])
+        |> send_resp(200, contents)
+      _ ->
+        conn
+        |> redirect(to: file_path(conn, :index))
+    end
+  end
+
+  def edit(conn, %{"id" => id}) do
+    current_user_id = Coherence.current_user(conn).id
+    file = Repo.get!(DFile, id)
+    changeset = DFile.changeset(file)
+    render(conn, "edit.html", file: file, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "d_file" => file_params}) do
+    current_user_id = Coherence.current_user(conn).id
+    file = Repo.get!(DFile, id)
+    changeset = DFile.changeset(file, file_params)
+
+    case Repo.update(changeset) do
+      {:ok, _file} ->
+        conn
+        |> put_flash(:info, "File updated successfully.")
+        |> redirect(to: file_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "edit.html", file: file, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    current_user_id = Coherence.current_user(conn).id
+    file = Repo.get!(DFile, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(file)
+
+    conn
+    |> put_flash(:info, "User deleted successfully.")
+    |> redirect(to: file_path(conn, :index))
+  end
+
+
 end
